55af42cb4e824e09aa394fc58dc6875b8c5ff393
Louis_Grant <ljsgrant@gmail.com>
2022-11-15 21:16:16 +0000

    working on styling: container dimensions

--- scripts/practice.js
+++
@@ -1,9 +0,0 @@
-function clearRows() {
-  for (let index = 296; index > 0; index -= 14) {
-    // const rowToCheck = gridSquares.slice(index, index + 10);
-    // console.log(rowToCheck);
-    console.log(index);
-  }
-}
-
-clearRows();
